f37ddbff5667e95c37ece01abca81621959556d3
cmaran <cmaran@student.tgm.ac.at>
2014-10-20 11:00:51 +0200

    fixed walk file

--- Get.py
+++ Get.py
@@ -3,10 +3,9 @@ from pysnmp.entity.rfc3413.oneliner import cmdgen
 cmdGen = cmdgen.CommandGenerator()
 
 errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
-    cmdgen.CommunityData('public'),
-    cmdgen.UdpTransportTarget(('demo.snmplabs.com', 161)),
-    '1.3.6.1.2.1.1.1.0',
-    '1.3.6.1.2.1.1.6.0'
+    cmdgen.CommunityData('5xHI'),
+    cmdgen.UdpTransportTarget(('10.0.100.10', 161)),
+    '1.3.6.1.4.1.3224.10.1'
 )
 
 # Check for errors and print out results
